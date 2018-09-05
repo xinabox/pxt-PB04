dbf9fba88d95b12a54e71891b06aad941ec91804
brendanvanbreda <brendanvanbreda1993@gmail.com>
2018-09-05 16:57:45 +0200

    add source code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pxt-PB04
 MakeCode package for the ☒PB04
+
+## Getting Started
+
+1. Open [Microsoft MakeCode for micro:bit](https://makecode.microbit.org).
+2. Select *Advanced > Add Package* in the toolbox.
+3. Copy and paste **https://github.com/xinabox/pxt-PB04** in the search bar and click search
+4. Select the package by clicking on it. The package will appear along with the existing packages in the toolbox.
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+```package
+PB04=github:xinabox/pxt-PB04
+```
---
+++ pb04.ts
@@ -0,0 +1,72 @@
+/**
+ * PB04 Intelligent Battery Module
+ */
+//% weight=10 color=#9F79EE icon="\uf108" block="PB04"
+namespace PB04 {
+    // Device I2C Address
+    const MAX11646_I2C_ADDRESS = 0x36
+
+    // Register Defines
+    const MAX11646_REG_SETUP = 0x80
+    const MAX11646_REG_CONF = 0x00
+
+    // System Defines
+    const MAX11646_VOLTAGE =0x21
+    const MAX11646_CURRENT = 0x23
+    const MAX11646_SEL0 = 0x40
+    const MAX11646_SEL1 = 0x20
+    const MAX11646_SEL2 = 0x10
+    const MAX11646_CLK_EX = 0x08
+    const MAX11646_BIP_ON = 0x04
+    const MAX11646_RST = 0x02
+    const MAX11646_SCAN0 = 0x40
+    const MAX11646_SCAN1 = 0x20
+    const MAX11646_CH_0 = 0x00
+    const MAX11646_CH_1 = 0x02
+    const MAX11646_MODE_SINGLE = 0x01
+    const MAX11646_SETUP = 0xD2
+
+    function readCurrent(): number {
+        let var1 = modeADC(MAX11646_CURRENT);
+        let var2 = (var1 & 0xFF00) >> 8;
+        let var3 = (var1 & 0x00FF);  
+        let current = (((((((var2 - 252)*256) + (var3-1))*2.048)/1024)/100)/0.02)*1000;
+        return current;
+    }
+
+    function readVoltage(): number {
+        let var1 = modeADC(MAX11646_VOLTAGE);
+        let var2 = (var1 & 0xFF00) >> 8;
+        let var3 = (var1 & 0x00FF);       
+        let voltage = (((((var2 - 252)*256) + var3)*2.048)/1024)*2;
+        return voltage;
+    }
+
+    function modeADC(data: number): number {
+        let buf: Buffer = pins.createBuffer(2);
+        buf[0] = MAX11646_SETUP;
+        buf[1] = data;
+        pins.i2cWireBuffer(MAX11646_I2C_ADDRESS, buf, false);
+        let data = pins.i2cReadNumber(MAX11646_I2C_ADDRESS, NumberFormat.UInt16LE, false);
+        return data;
+    }
+
+    /**
+	* PB04 Voltage (V)
+	*/
+    //% blockId="Voltage" block="Voltage (V)"
+    //% blockGap=1 weight=90
+    export function getVoltage(): number {
+        return readVoltage();
+    }
+
+    /**
+	* PB04 Current (mA)
+	*/
+    //% blockId="Current" block="Current (mA)"
+    //% blockGap=1 weight=90
+    export function getCurrent(): number {
+        return readCurrent();
+    }
+
+}
