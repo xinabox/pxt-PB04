2f5eaf605df30b60490a87efd222cb8a342e7952
KalbeAbbas <kalbeabbas142@gmail.com>
2019-11-27 13:01:09 +0500

    Updates.

--- pb04.ts
+++ pb04.ts
@@ -123,30 +123,30 @@ namespace PB04 {
         } else if (current > 100 && current < 500) {
             if (voltage >= 3.0) percentage_100 = 100
             else if (voltage > 2.8 && voltage < 3.0) percentage_100 = 90
-            else if (voltage > 2.6 && voltage < 2.8) percentage_100 = 80
-            else if (voltage > 2.6 && voltage < 2.7) percentage_100 = 70
-            else if (voltage == 2.6) percentage_100 = 60
-            else if (voltage > 2.5 && voltage < 2.6) percentage_100 = 50
-            else if (voltage == 2.4) percentage_100 = 40
-            else if (voltage > 2.3 && voltage < 2.4) percentage_100 = 30
-            else if (voltage > 2.2 && voltage < 2.3) percentage_100 = 20
-            else if (voltage > 2.0 && voltage < 2.2) percentage_100 = 10
-            else if (voltage < 2.0) percentage_100 = 0
+            else if (voltage > 2.6 && voltage <= 2.8) percentage_100 = 80
+            else if (voltage > 2.6 && voltage <= 2.7) percentage_100 = 70
+            else if (voltage > 2.55 && voltage <= 2.6) percentage_100 = 60
+            else if (voltage > 2.5 && voltage <= 2.55) percentage_100 = 50
+            else if (voltage > 2.45 && voltage <= 2.5) percentage_100 = 40
+            else if (voltage > 2.3 && voltage <= 2.4) percentage_100 = 30
+            else if (voltage > 2.2 && voltage <= 2.3) percentage_100 = 20
+            else if (voltage > 2.0 && voltage <= 2.2) percentage_100 = 10
+            else if (voltage <= 2.0) percentage_100 = 0
 
             return percentage_100
 
         } else if (current > 500) {
             if (voltage >= 2.95) percentage_500 = 100
             else if (voltage > 2.8 && voltage < 2.95) percentage_500 = 90
-            else if (voltage > 2.6 && voltage < 2.8) percentage_500 = 80
-            else if (voltage > 2.5 && voltage < 2.6) percentage_500 = 70
-            else if (voltage > 2.4 && voltage < 2.5) percentage_500 = 60
-            else if (voltage > 2.3 && voltage < 2.4) percentage_500 = 50
-            else if (voltage > 2.2 && voltage < 2.3) percentage_500 = 40
-            else if (voltage == 2.2) percentage_500 = 30
-            else if (voltage > 2.1 && voltage < 2.2) percentage_500 = 20
-            else if (voltage < 2.1 && voltage > 2.0) percentage_500 = 10
-            else if (voltage < 2.0) percentage_500 = 0
+            else if (voltage > 2.6 && voltage <= 2.8) percentage_500 = 80
+            else if (voltage > 2.5 && voltage <= 2.6) percentage_500 = 70
+            else if (voltage > 2.4 && voltage <= 2.5) percentage_500 = 60
+            else if (voltage > 2.3 && voltage <= 2.4) percentage_500 = 50
+            else if (voltage > 2.2 && voltage <= 2.3) percentage_500 = 40
+            else if (voltage > 2.15 && voltage <= 2.2) percentage_500 = 30
+            else if (voltage > 2.1 && voltage <= 2.15) percentage_500 = 20
+            else if (voltage > 2.0 && voltage <= 2.1) percentage_500 = 10
+            else if (voltage <= 2.0) percentage_500 = 0
 
             return percentage_500
         } else {
